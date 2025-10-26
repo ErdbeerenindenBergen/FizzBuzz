02024d434d7dbba4c14ab727d2f54a45a095fb1f
ErdbeerenindenBergen <114823741+ErdbeerenindenBergen@users.noreply.github.com>
2025-10-26 19:18:39 -0400

    Create project

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,21 @@
+import org.example.FizzBuzz;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void shouldReturnAnInstanceOfFizzBuzz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertNotNull(fizzBuzz);
+    }
+
+
+
+
+}
+
+
+
+
