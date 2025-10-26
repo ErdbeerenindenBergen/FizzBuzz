82c761da83178b4fd804f4f20b8180d8e054e663
ErdbeerenindenBergen <114823741+ErdbeerenindenBergen@users.noreply.github.com>
2025-10-26 19:45:56 -0400

    add minimal code to return Buzz when input is divisible by 5

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class FizzBuzz {
     public String calculate(int i) {
+        if (i % 5 == 0) {
+            return "Buzz";
+        }
         return "Fizz";
     }
 }
