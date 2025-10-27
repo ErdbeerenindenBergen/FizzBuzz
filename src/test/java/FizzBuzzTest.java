ba89c689b47b0ea944a87e0f6072a125a2a048bc
ErdbeerenindenBergen <114823741+ErdbeerenindenBergen@users.noreply.github.com>
2025-10-26 21:27:01 -0400

    Expand test cases

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -5,33 +5,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FizzBuzzTest {
 
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
     @Test
     public void shouldReturnAnInstanceOfFizzBuzz() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertNotNull(fizzBuzz);
     }
 
     @Test
     public void shouldReturnFizzWhenInputIsDivisibleBy3() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("Fizz", fizzBuzz.calculate(3));
+        assertEquals("Fizz", fizzBuzz.calculate(6));
+        assertEquals("Fizz", fizzBuzz.calculate(9));
     }
 
     @Test
     public void shouldReturnBuzzWhenInputIsDivisibleBy5() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("Buzz", fizzBuzz.calculate(5));
     }
 
     @Test
     public void shouldReturnFizzBuzzWhenInputIsDivisibleBy3And5() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("FizzBuzz", fizzBuzz.calculate(15));
     }
 
     @Test
     public void shouldReturnIntegerWhenInputIsNotDivisibleBy3Or5() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("2", fizzBuzz.calculate(2));
     }
 }
