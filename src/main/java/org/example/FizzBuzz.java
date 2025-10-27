fc0b03f86df2a8e1939e3bf174bc06ecf5439e6a
ErdbeerenindenBergen <114823741+ErdbeerenindenBergen@users.noreply.github.com>
2025-10-26 20:16:09 -0400

    Add minimal logic to return FizzBuzz if input is divisible by 3 and 5

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class FizzBuzz {
     public String calculate(int i) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        }
         if (i % 5 == 0) {
             return "Buzz";
         }
